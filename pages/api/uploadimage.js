55ff3f85423c79b1c249c2821326acb2d15716f5
unknown <kingsimpa69@gmail.com>
2024-03-24 16:46:09 -0600

    Trying to fix imgur upload from non residetial IP

--- pages/api/uploadimage.js
+++ pages/api/uploadimage.js
@@ -3,12 +3,13 @@ const uploadimage = async (req, res) => {
       try {
         const { imageData } = req.body;
         const imgurClientId = process.env.IMGUR_CLIENT_ID;
-  
+        const clientIpAddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
         const response = await fetch('https://api.imgur.com/3/image', {
           method: 'POST',
           headers: {
             'Authorization': `Client-ID ${imgurClientId}`,
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'X-Forwarded-For': clientIpAddress
           },
           body: JSON.stringify({ image: imageData })
         });
@@ -20,11 +21,11 @@ const uploadimage = async (req, res) => {
           res.status(200).json({ imageUrl });
         } else {
           console.error('Error uploading image to Imgur:', data);
-          res.status(500).json({ error: 'Error uploading image to Imgur' });
+          res.status(500).json({ error: data });
         }
       } catch (error) {
         console.error('Error uploading image to Imgur:', error);
-        res.status(500).json({ error: 'Error uploading image to Imgur' });
+        res.status(500).json({ error: data });
       }
     } else {
       res.setHeader('Allow', ['POST']);
